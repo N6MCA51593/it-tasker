3590194c72264ecb2731c2e40cee96a4006ae36a
K.C <chesnokovks@gmail.com>
2021-04-12 21:15:53 +0300

    landing page progress

--- client/src/features/landing/LandingPage.js
+++ client/src/features/landing/LandingPage.js
@@ -1,39 +1,22 @@
-import React, { useState } from 'react';
-import useInput from 'common/useInput';
-import { useDispatch } from 'react-redux';
-import { login } from 'features/api/login';
-import { signup } from 'features/api/signup';
+import Container from 'features/landing/Container';
+import React from 'react';
+import image from 'features/landing/pic.jpg';
 
 const LandingPage = () => {
-  const [isLogin, setIsLogin] = useState(true);
-  const { value: userName, bind: bindUserName } = useInput('');
-  const { value: password, bind: bindPassword } = useInput('');
-  const dispatch = useDispatch();
-
-  const handleClick = () => {
-    if (userName.length > 0 && password.length > 0) {
-      if (isLogin) {
-        dispatch(login({ userName, password }));
-      } else {
-        dispatch(signup({ userName, password }));
-      }
-    }
-  };
-
   return (
     <div className='landing'>
-      <label>
-        Username:
-        <input {...bindUserName} />
-      </label>
-      <label>
-        Password:
-        <input {...bindPassword} />
-      </label>
-      <button onClick={() => handleClick()}>
-        {isLogin ? 'Login' : 'Signup'}
-      </button>
-      <button onClick={() => setIsLogin(!isLogin)}>Switch</button>
+      <div className='landing-container'>
+        <div className='img-container'>
+          <div className='img-text'>
+            <h1>IT task manager</h1>
+            <h2>
+              Interactive workplace map and task tracker for IT professionals
+            </h2>
+          </div>
+          <img src={image} alt='pic' />
+        </div>
+        <Container />
+      </div>
     </div>
   );
 };
